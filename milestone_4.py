061c296b8901368d77b750eb8c616fdd61aae3ae
Johnstone <nj0017@surrey.ac.uk>
2024-05-25 10:44:24 +0100

    Completed hangman class.

--- milestone_4.py
+++ milestone_4.py
@@ -12,22 +12,30 @@ class Hangman:
         
     def check_guess(self, guess):
         # Convert the guessed letter to lower case
-        self.guess = guess.lower()
+        guess = guess.lower()
          # Create an if statement that checks if the guess is in the word
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            for i, letter in enumerate(self.word):
+                if guess == letter:
+                    self.word_guessed[i] = guess
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
 
-
+    
+       
     def ask_for_input(self):
         while True:
             guess = input("Please guess a letter:") 
-            if len(guess) > 1 and not guess.isalpha():
+            if len(guess) != 1 or not guess.isalpha():
                 print("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in guess_list():
+            elif guess in self.list_of_guesses():
                 print("You already tried that letter!")
             else:
                 self.list_of_guesses.append(guess)
                 self.check_guess(guess)    
 
-    
-
+# %%
